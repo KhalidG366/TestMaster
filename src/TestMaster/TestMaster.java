ce4090035cb279bdd6796128fc62aa231bc224b7
Admin <Admin@DESKTOP-QI6H2EA>
2024-08-21 21:22:32 +0530

    Hello check

---
+++ src/TestMaster/TestMaster.java
@@ -0,0 +1,10 @@
+package TestMaster;
+
+public class TestMaster {
+
+	public static void main(String[] args) 
+	{
+	System.out.println("Hello first check");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module TestMaster {
+}
